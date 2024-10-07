ba9e3d23da1de0a1d4730c5acf160d02d9e8f2e6
BIsetsuko <seina.nagato@blueish.co.jp>
2024-10-07 23:00:26 +0900

    feat:シンプルなフォームをコーディング

---
+++ src/app/simple/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+  return (
+    <div className="w-2/4 mt-72 m-auto">
+      <form className="flex gap-3">
+        <input
+          type="text"
+          name="name"
+          className="w-96 border border-neutral-600 bg-blue-50 rounded-md p-2"
+        />
+        <button type="submit" className="bg-blue-700 text-white p-2 rounded-md">
+          送信
+        </button>
+      </form>
+    </div>
+  );
+}
