39dab5d10ce6b4a7c59c6acd2314fe32007fb939
BIsetsuko <seina.nagato@blueish.co.jp>
2024-10-07 23:37:10 +0900

    feat:リダイレクトページを作成して、送信ボタンでフォームデータ取得後リダイレクトする

--- src/app/simple/postAction.ts
+++ src/app/simple/postAction.ts
@@ -1,7 +1,11 @@
 "use server";
 
+import { redirect } from "next/navigation";
+
 export async function postAction(formData: FormData) {
   const name = formData.get("name");
   console.log(name);
   // 必要な処理を書く
+  // 例えばリダイレクト
+  redirect("/simple/thanks");
 }
---
+++ src/app/simple/thanks/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="w-2/4 mt-72 m-auto">
+      <p>リダイレクト成功</p>
+      <p>Thanksページです</p>
+    </div>
+  );
+}
